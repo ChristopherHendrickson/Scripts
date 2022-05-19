6029023320ed1157a8237f9600cc55cf6d3d880a
Chris <chris.hendricksonacc@gmail.com>
2022-05-20 00:07:55 +1200

    commit

--- generateParenthesis.py
+++ generateParenthesis.py
@@ -7,19 +7,19 @@ def possibles(n,possibles_lst=[],lst=[]):
     #Generates all possible combinations of opening and closing brackets of length n
     #a value of 1 represents an open bracket, -1 a closing bracket
     #This works recursively. Each iteration the function makes two new lists from the input by appending a 1 and -1.
-    #Then checks if the list is of length n. if it is, it appends it to the output list, otherwise it calls itself again twice with the new lists
-    if len(lst)<=n: 
-        lst_neg = lst[:]
-        lst_neg.append(-1)
-        lst_pos = lst[:]
-        lst_pos.append(1)
-        if len(lst_pos) == n:
-            possibles_lst.append(lst_pos)
-            possibles_lst.append(lst_neg)
+    #Then checks if the list is of length n. if it is, it appends it to the output list, otherwise it calls itself again with the new lists
+    lst_neg = lst[:]
+    lst_neg.append(-1)
+    lst_pos = lst[:]
+    lst_pos.append(1)
+    if len(lst_pos) == n:
+        possibles_lst.append(lst_neg)
+        possibles_lst.append(lst_pos)
+    else:
         possibles(n,possibles_lst,lst_neg)
         possibles(n,possibles_lst,lst_pos)
-
     return possibles_lst
+    
 
 
 def valid(lst):
@@ -29,7 +29,7 @@ def valid(lst):
         sum_tot = 0
         for value in i:
             sum_tot += value
-            if sum_tot < 0:
+            if sum_tot < 0: #Means a close bracket occured without a previous open bracket
                 break
         if sum_tot == 0:
             for [counter,value] in enumerate(i):
@@ -46,5 +46,5 @@ def generateParenthesis(n):
 
 
 
-print(generateParenthesis(4))
-
+#print(generateParenthesis(6))
+print(possibles(4))
