03488409acad549d25892fd50b116c8c040c7dfe
Chris <chris.hendricksonacc@gmail.com>
2022-07-24 14:48:37 +1200

    commit;

--- anagram.py
+++
@@ -1,6 +0,0 @@
-#Valid anagram
-def anagram(str1,str2):
-  return sorted(str1.lower())==sorted(str2.lower())
-
-print(anagram('daNger','garden'))
-
--- kthLargestElement.py
+++
@@ -1,7 +0,0 @@
-#Kth largest element
-
-def kth(arr,k):
-    return sorted(arr)[-k]
-
-
-print(kth([1,5,3,6,6,4,7,8,4,3,2,78,45,3],4))
